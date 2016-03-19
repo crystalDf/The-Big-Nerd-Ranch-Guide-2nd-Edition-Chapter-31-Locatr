29fb423e83530fe969a7027218b2aa9e7b6d4a2b
crystalDf <chendong333@gmail.com>
2016-03-19 23:26:50 +0800

    Chapter 31 Locatr

--- app/src/main/java/com/star/locatr/LocatrFragment.java
+++ app/src/main/java/com/star/locatr/LocatrFragment.java
@@ -11,9 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 public class LocatrFragment extends Fragment {
 
     private ImageView mImageView;
+    private GoogleApiClient mGoogleApiClient;
 
     public static LocatrFragment newInstance() {
         return new LocatrFragment();
@@ -24,6 +28,11 @@ public class LocatrFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .build();
+
     }
 
     @Nullable
